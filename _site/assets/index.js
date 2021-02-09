ab91884979167c8dd910d05e1b0028028f9c2d0f
Work <WorkAccount@Vickys-MBP.hsd1.ma.comcast.net>
2021-02-09 00:03:10 -0500

    Project layout

--- _site/assets/index.js
+++ _site/assets/index.js
@@ -6,7 +6,7 @@
 var countDownDate = new Date("Apr 27, 2021 09:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
   // Get today's date and time
   var now = new Date().getTime();
